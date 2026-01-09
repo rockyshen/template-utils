d9beabdf1e0e1a0c2b22f06bc28cc5f0295107c9
rockyshen <875430544@qq.com>
2026-01-09 10:03:54 +0800

    feat: 自定义脱敏注解@Desensitization，新增注释说明

--- src/main/java/com/rockyshen/annotation/Desensitization.java
+++ src/main/java/com/rockyshen/annotation/Desensitization.java
@@ -16,23 +16,11 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-/**
- * @JacksonAnnotationsInside 是 Jackson 库中一个非常有用的注解，它允许我们创建包含多个 Jackson 注解的自定义注解。
- * 这使得我们在使用这些自定义注解时，可以像使用单个注解一样方便地应用多个序列化/反序列化规则
- * 即该自定义注解可以包含多个 Jackson 提供的注解，并在被应用到类、方法或字段上时，这些包含的注解会被内联应用到目标元素上
- */
-@JacksonAnnotationsInside
-/**
- * @JsonSerialize 是 Jackson 库中的一个注解，用于指定在将 Java 对象序列化为 JSON 时使用的自定义序列化器。
- * 通过该注解，你可以为特定的类、字段或 getter 方法指定一个自定义的序列化逻辑，
- * 从而控制对象在序列化为 JSON 时的表现形式。
- */
-@JsonSerialize(using = DesensitizationJsonSerializable.class)
+@JacksonAnnotationsInside    // 让 自定义注解 能够"继承"其上标注的所有 Jackson 注解。
+@JsonSerialize(using = DesensitizationJsonSerializable.class)  // 指定使用哪个自定义序列化器来处理被标注的字段。
 public @interface Desensitization {
     /**
      * 脱敏策略
-     *
-     * @return
      */
     DesensitizationStrategyEnum value();
 }
--- src/main/java/com/rockyshen/utils/DesensitizationJsonSerializable.java
+++ src/main/java/com/rockyshen/utils/DesensitizationJsonSerializable.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 /**
+ * 这个自定义的Jackson序列化器，必须是JsonSerializer的子类，这样Jackson才认
  * @author rockyshen
  * @date 2026/1/8 23:05
  */
@@ -20,13 +21,10 @@ public class DesensitizationJsonSerializable extends JsonSerializer<String> impl
     // 基于枚举项的脱敏策略
     private DesensitizationStrategyEnum desensitizationStrategy;
 
-    @Override
-    public void serialize(String s, JsonGenerator gen, SerializerProvider serializers) throws IOException
-    {
-        // 使用脱敏策略将字符串处理后序列化到json中
-        gen.writeString(desensitizationStrategy.getDesensitization().apply(s));
-    }
-
+    /**
+     * 实现自：【接口】ContextualSerializer
+     * 作用是：在序列化之前被调用，用于根据字段的上下文信息（如注解）创建定制化的序列化器。
+     */
     @Override
     public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property)
             throws JsonMappingException
@@ -45,4 +43,14 @@ public class DesensitizationJsonSerializable extends JsonSerializer<String> impl
         return prov.findValueSerializer(property.getType(), property);
     }
 
+    /**
+     * 重写自： 【抽象类】JsonSerializer
+     * 执行序列化
+     */
+    @Override
+    public void serialize(String s, JsonGenerator gen, SerializerProvider serializers) throws IOException
+    {
+        // 使用脱敏策略将字符串处理后序列化到json中
+        gen.writeString(desensitizationStrategy.getDesensitization().apply(s));
+    }
 }
